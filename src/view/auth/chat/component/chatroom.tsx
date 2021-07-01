c42b8e5b5082f7342ba8d9ba1952de78c166e2a5
MaxCSH <maxchen.sihhan@gmail.com>
2021-07-01 15:27:17 +0800

    chatroom load

--- src/view/auth/chat/component/chatroom.tsx
+++ src/view/auth/chat/component/chatroom.tsx
@@ -3,19 +3,18 @@ import { getMessages } from "../../../../services/userService";
 import Message from "../../../../interface/IMessage";
 import User from "../../../../interface/IUser";
 
-
-
 type ChatroomProps = {
-  userSelected?:User;
+  userSelected?: User;
   myUserName: string;
-  selfIntro?:string;
+  selfIntro?: string;
 };
 
-const Chatroom = ({  myUserName,userSelected }: ChatroomProps) => {
+const Chatroom = ({ myUserName, userSelected }: ChatroomProps) => {
   // const [keyPress,setKeyPress] = useState('');
 
-
-  const [forwardingUser, setForwardingUser] = useState<User>(userSelected || {} as User);
+  const [forwardingUser, setForwardingUser] = useState<User>(
+    userSelected || ({} as User)
+  );
 
   const [messages, setMes] = useState<Message[]>([]);
   const [dateChecker, setDateChecker] = useState<Date>();
@@ -24,7 +23,7 @@ const Chatroom = ({  myUserName,userSelected }: ChatroomProps) => {
     setForwardingUser(userSelected!);
     setMes([]);
     getMessages().subscribe((res) => {
-      console.log("Success =>",res);
+      console.log("Success =>", res);
       setMes(res);
     });
   }, [userSelected]);
@@ -56,14 +55,22 @@ const Chatroom = ({  myUserName,userSelected }: ChatroomProps) => {
   const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
       if (inputValue.match(/^(?!\s*$).+/)) {
-        console.log("1 ",new Date(),"2 " ,messages[messages.length-1]?.date)
+        console.log(
+          "1 ",
+          new Date(),
+          "2 ",
+          messages[messages.length - 1]?.date
+        );
         setMes([
           ...messages,
           {
             username: myUserName,
             message: inputValue,
             date: new Date().getTime(),
-            timeHint:(new Date().getTime() - messages[messages.length-1]?.date)/(1000*60) > 5
+            timeHint:
+              (new Date().getTime() - messages[messages.length - 1]?.date) /
+                (1000 * 60) >
+              5,
           } as Message,
         ]);
         setInputValue("");
@@ -82,16 +89,16 @@ const Chatroom = ({  myUserName,userSelected }: ChatroomProps) => {
     //   return [mesDate].map(ele => `${ele.toLocaleDateString()}  ${ele.toLocaleTimeString([],{ hour: '2-digit', minute: '2-digit' })}`)
     // }
     // return "";
-    return ele.timeHint?[mesDate].map(
-      (ele) =>
-        `${ele.toLocaleDateString()}  ${ele.toLocaleTimeString([], {
-          hour: "2-digit",
-          minute: "2-digit",
-        })}`
-    ):"";
+    return ele.timeHint
+      ? [mesDate].map(
+          (ele) =>
+            `${ele.toLocaleDateString()}  ${ele.toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            })}`
+        )
+      : "";
   };
-
-  
 
   const starterTemplate = (
     <div className="flex-grow w-screen sm:w-160 bg-white  border flex flex-col items-center justify-center text-2xl">
@@ -100,83 +107,86 @@ const Chatroom = ({  myUserName,userSelected }: ChatroomProps) => {
   );
 
   const messagesError = (
-    <div className="flex-grow animate-pulse bg-white  flex flex-col items-center justify-center text-2xl">
-      {[...Array(10)].map((ele, index) => (
-              <div className="w-full">
-                <div
-                  className={`flex w-full `}
-                  id={`message_${index}`}
-                  key={`message_${index}`}
-                >
-                  <div
-                    className={`flex my-2 w-full`}
-                  >
-                    <div
-                      className="h-10 w-10 border rounded-full bg-gray-400"
-                    />
-                    <div
-                      className={`mx-2  w-5/6`}
-                    >
-                      <div className="h-14 w-5/6 px-4 py-3 max-w-md flex flex-wrap break-all  items-center justify-around border rounded-3xl">
-                      </div>
-                    </div>
-                  </div>
+    <div className="animate-puls">
+      {[...Array(11)].map((ele, index) => (
+        <div className="w-full">
+          <div
+            className={`flex w-full `}
+            id={`message_${index}`}
+            key={`message_${index}`}
+          >
+            <div className={`flex my-2 w-full`}>
+              <div className="h-10 w-10  rounded-full bg-gray-200" ></div>
+              <div className={`mx-2  w-5/6`}>
+                <div className="h-14 w-5/6 px-4 py-3 max-w-md bg-gray-200 flex flex-wrap break-all  items-center justify-around border rounded-3xl">
                 </div>
               </div>
-            ))
-                    }
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+
+
+  const messagesList = (
+    <div>
+      {messages.map((ele, index) => (
+        <div>
+          <div className="text-center text-xs text-gray-600 my-4">
+            {dateController(ele)}
+          </div>
+
+          <div
+            className={`flex w-full ${
+              ele.username === myUserName ? "flex-row-reverse" : ""
+            }`}
+            id={`message_${index}`}
+            key={`message_${index}`}
+
+            // ref={(el) => {
+            //   messagesEnd = el!;
+            // }}
+          >
+            <div
+              className={`flex my-2 max-w-lg ${
+                ele.username === myUserName ? "flex-row-reverse" : ""
+              }`}
+            >
+              <img
+                className="h-10 w-10 border rounded-full"
+                alt=""
+                src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-2-800x800.jpg"
+              />
+              <div
+                className={`mx-2 ${
+                  ele.username === myUserName ? " text-right" : ""
+                }`}
+              >
+                <div className="px-4 py-3 max-w-md flex flex-wrap break-all  items-center justify-around border rounded-3xl">
+                  {ele.message}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 
   const chatroomTemplate = (
     <div className="flex-grow w-screen sm:w-160 bg-white  border flex flex-col">
-      <div className="h-20 w-full flex flex-col items-center justify-center py-2 px-8 border-b">
-        <div className="w-full flex text-lg font-semibold">{forwardingUser?forwardingUser.username :"Select a user"}</div>
-        <div className="w-full text-sm">{forwardingUser?forwardingUser.intro: "text"}</div>
+      <div className="h-18 w-full flex flex-col items-center justify-center py-2 px-8 border-b">
+        <div className="w-full flex text-lg font-semibold">
+          {forwardingUser ? forwardingUser.username : "Select a user"}
+        </div>
+        <div className="w-full text-sm">
+          {forwardingUser ? forwardingUser.intro : "text"}
+        </div>
       </div>
       <div className="flex flex-col flex-grow px-4 overflow-y-scroll">
-        {messages
-          ? messages.map((ele, index) => (
-              <div>
-                <div className="text-center text-xs text-gray-600 my-4">
-                  {dateController(ele)}
-                </div>
-
-                <div
-                  className={`flex w-full ${
-                    ele.username === myUserName ? "flex-row-reverse" : ""
-                  }`}
-                  id={`message_${index}`}
-                  key={`message_${index}`}
-
-                  // ref={(el) => {
-                  //   messagesEnd = el!;
-                  // }}
-                >
-                  <div
-                    className={`flex my-2 max-w-lg ${
-                      ele.username === myUserName ? "flex-row-reverse" : ""
-                    }`}
-                  >
-                    <img
-                      className="h-10 w-10 border rounded-full"
-                      alt=""
-                      src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-2-800x800.jpg"
-                    />
-                    <div
-                      className={`mx-2 ${
-                        ele.username === myUserName ? " text-right" : ""
-                      }`}
-                    >
-                      <div className="px-4 py-3 max-w-md flex flex-wrap break-all  items-center justify-around border rounded-3xl">
-                        {ele.message}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))
-          : messagesError}
+        {messages && messages.length > 0 ? messagesList : ""}
+        {messages && messages.length > 0 ? "" : messagesError}
       </div>
       <div className="h-14 py-2 flex items-center px-4">
         <div className="w-full h-10 px-4 border rounded-full flex items-center ">
